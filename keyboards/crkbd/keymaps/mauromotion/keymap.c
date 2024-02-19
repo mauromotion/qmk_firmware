5143ebd08712e43e399446b77723342aadff7aa8
mauromotion <hello@mauromotion.com>
2024-02-19 23:31:44 +0000

    added combos for parentheses, RShift on thumb

--- keyboards/crkbd/keymaps/mauromotion/keymap.c
+++ keyboards/crkbd/keymaps/mauromotion/keymap.c
@@ -26,9 +26,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_LSFT,    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        LCTL_T(KC_TAB), LALT_T(KC_Z), KC_X, KC_C, KC_V, KC_B,                       KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+        LCTL_T(KC_TAB), LALT_T(KC_Z), KC_X, KC_C, KC_V, KC_B,                       KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH, TT(04),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                      KC_LGUI, LT(1,KC_TAB), KC_SPC,    KC_ENT,   LT(2,KC_BSPC),   TT(4)
+                                      KC_LGUI, LT(1,KC_TAB), KC_SPC,    KC_ENT,   LT(2,KC_BSPC),   KC_RSFT
                                       //`--------------------------'  `--------------------------'
     ),
 
@@ -48,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_ESC,   KC_1,   KC_2,   KC_3,    KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,  KC_4,   KC_5,   KC_6,    KC_0,  XXXXXXX,                     KC_BSLS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_GRV,
+      KC_LSFT,  KC_4,   KC_5,   KC_6,    KC_0,  XXXXXXX,                        KC_BSLS, KC_MINS, KC_EQL,  KC_PIPE, XXXXXXX, KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    LCTL_T(KC_TAB), KC_7, KC_8, KC_9,    KC_LCBR,  KC_RCBR,                     KC_PIPE, KC_UNDS, KC_PLUS, XXXXXXX, XXXXXXX, KC_NUBS,
+    LCTL_T(KC_TAB), KC_7, KC_8, KC_9,    KC_LBRC,  KC_LCBR,                     KC_RCBR, KC_RBRC, KC_COMM, KC_DOT, XXXXXXX, KC_NUBS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   MO(3),    KC_SPC,    KC_ENT,  _______, KC_LALT
                                       //`--------------------------'  `--------------------------'
@@ -74,18 +74,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        KC_TAB,  KC_Q,   KC_W,     KC_E,    KC_R,   KC_T,                         KC_F1,  KC_PGUP,  KC_PGDN,  KC_L,   KC_SCLN, KC_PIPE,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,  KC_A,   KC_S,     KC_D,    KC_F,   KC_G,                         KC_N,   KC_M,     KC_COMM,  KC_DOT, KC_SLSH, KC_F12,
+      KC_LSFT,  KC_A,   KC_S,     KC_D,    KC_F,   KC_G,                         KC_N,   KC_M,     KC_COMM,  KC_DOT, KC_SLSH, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LCTL,   KC_C,  KC_SPC,    KC_PENT,  KC_UP,  _______
+                                          KC_LCTL,   KC_C,  KC_SPC,    KC_PENT,  KC_UP,  KC_F12
                                       //`--------------------------'  `--------------------------'
     )
 };
 
-// Combos
+// Combos //
 const uint16_t PROGMEM jk_combo[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM rt_combo[] = {KC_R, KC_T, COMBO_END};
+const uint16_t PROGMEM fg_combo[] = {KC_F, KC_G, COMBO_END};
+const uint16_t PROGMEM vb_combo[] = {KC_V, KC_B, COMBO_END};
+const uint16_t PROGMEM yu_combo[] = {KC_Y, KC_U, COMBO_END};
+const uint16_t PROGMEM hj_combo[] = {KC_H, KC_J, COMBO_END};
+const uint16_t PROGMEM nm_combo[] = {KC_N, KC_M, COMBO_END};
 
 combo_t key_combos[] = {
-    COMBO(jk_combo, KC_ESC)
+    COMBO(jk_combo, KC_ESC),
+    COMBO(rt_combo, KC_LPRN),
+    COMBO(fg_combo, KC_LBRC),
+    COMBO(vb_combo, KC_LCBR),
+    COMBO(yu_combo, KC_RPRN),
+    COMBO(hj_combo, KC_RBRC),
+    COMBO(nm_combo, KC_RCBR)
 };
 // End of Combos //
 
