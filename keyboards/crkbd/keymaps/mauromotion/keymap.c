ed20943c93dfeaf4beca68af1b04b263268328a0
mauromotion <hello@mauromotion.com>
2024-03-10 15:15:27 +0000

    Reverted Alt and Gui positions

--- keyboards/crkbd/keymaps/mauromotion/keymap.c
+++ keyboards/crkbd/keymaps/mauromotion/keymap.c
@@ -58,11 +58,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[3] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        XXXXXXX, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F10,                    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, KC_F4,   KC_F5,   KC_F6,   KC_F11,                   KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NUM, KC_CAPS,
+        XXXXXXX, XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F12,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NUM, KC_CAPS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, _______, KC_SPC,     KC_ENT,  _______, KC_LALT
                                       //`--------------------------'  `--------------------------'
@@ -70,13 +70,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[4] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,                         KC_6,   KC_7,     KC_8,     KC_9,   KC_0,    KC_X,
+       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,                         KC_6,   KC_7,     KC_8,     KC_9,   KC_0,    KC_F12,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_TAB,  KC_Q,   KC_W,     KC_E,    KC_R,   KC_T,                         KC_F1,  KC_PGUP,  KC_PGDN,  KC_L,   KC_SCLN, KC_PIPE,
+       KC_TAB,  KC_Q,   KC_W,     KC_E,    KC_R,   KC_T,                         KC_F1,  KC_PGDN,  KC_PGUP,  KC_L,   KC_SCLN, KC_PIPE,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,  KC_A,   KC_S,     KC_D,    KC_F,   KC_G,                         KC_N,   KC_M,     KC_COMM,  KC_DOT, KC_SLSH, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LCTL,   KC_C,  KC_SPC,    KC_PENT,  KC_UP,  KC_F12
+                                          KC_LCTL,   KC_C,  KC_SPC,    KC_PENT,  KC_X,  KC_UP
                                       //`--------------------------'  `--------------------------'
     )
 };
