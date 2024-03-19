b06c0723762b089a1cccb85e4670bdfa0980de0d
mauromotion <hello@mauromotion.com>
2024-03-19 09:31:48 +0000

    First commit of the Home Row Mods layout variation

--- keyboards/crkbd/keymaps/mauromotion/keymap.c
+++ keyboards/crkbd/keymaps/mauromotion/keymap.c
@@ -20,19 +20,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <keymap_uk.h>
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_split_3x6_3(
+	[0] = LAYOUT_split_3x6_3( // Default
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         KC_ESC,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                           KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_LSFT,    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, RSFT_T(KC_QUOT),
+        KC_LSFT, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,      KC_H, RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        LCTL_T(KC_TAB), LALT_T(KC_Z), KC_X, KC_C, KC_V, KC_B,                       KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH, TT(04),
+        LCTL_T(KC_TAB), KC_Z, KC_X, KC_C, KC_V, KC_B,                       KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH, TT(04),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                       KC_LGUI, LT(1,KC_TAB), KC_SPC,    KC_ENT,   LT(2,KC_BSPC),   LALT_T(KC_DEL)
                                       //`--------------------------'  `--------------------------'
     ),
 
-	[1] = LAYOUT_split_3x6_3(
+	[1] = LAYOUT_split_3x6_3( // Lower (Navigation)
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
     LGUI(KC_TAB), XXXXXXX, LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), KC_BTN2,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_VOLD, KC_VOLU,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -44,19 +44,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
     ),
 
-	[2] = LAYOUT_split_3x6_3(
+	[2] = LAYOUT_split_3x6_3( // Raise (Numbers & Symbols)
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_PIPE,   KC_DLR,   KC_1,   KC_2,    KC_3,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,  KC_0,   KC_4,   KC_5,    KC_6,  KC_LBRC,                        KC_BSLS, KC_MINS, KC_EQL,  KC_PLUS, KC_GRV, KC_AT,
+      KC_LSFT,  KC_0,   KC_4,   KC_5,    KC_6,  KC_LBRC,                         KC_BSLS, RSFT_T(KC_MINS), RCTL_T(KC_EQL), LALT_T(KC_PLUS), RGUI_T(KC_GRV), KC_AT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    LCTL_T(KC_TAB), XXXXXXX, KC_7, KC_8,    KC_9,  KC_RBRC,                     KC_PIPE, KC_UNDS, KC_COMM, KC_DOT, XXXXXXX, KC_NUBS,
+    LCTL_T(KC_TAB), XXXXXXX, KC_7, KC_8,    KC_9,  KC_RBRC,                      KC_PIPE, KC_UNDS, KC_COMM, KC_DOT, XXXXXXX, KC_NUBS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   MO(3),    KC_SPC,    KC_ENT,  _______, KC_LALT
                                       //`--------------------------'  `--------------------------'
     ),
 
-	[3] = LAYOUT_split_3x6_3(
+	[3] = LAYOUT_split_3x6_3( // Adjust (Functions & Settings)
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         XXXXXXX, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F10,                    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
     ),
 
-	[4] = LAYOUT_split_3x6_3(
+	[4] = LAYOUT_split_3x6_3( // Gaming (FPS based on DayZ)
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,                         KC_6,   KC_7,     KC_8,     KC_9,   KC_0,    KC_F12,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
