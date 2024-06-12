f17be0ada33cd12ee32ea13c9aa3b3af53e47ffe
mauromotion <hello@mauromotion.com>
2024-06-12 18:39:35 +0100

    added tap dance Q as Esc to Ferris Sweep

---
+++ keyboards/ferris/keymaps/mauromotion/rules.mk
@@ -0,0 +1,13 @@
+BOOTLOADER = atmel-dfu
+DEFAULT_FOLDER = ferris/sweep
+
+RGBLIGHT_SUPPORTED = no
+RGB_MATRIX_SUPPORTED = no
+
+CAPS_WORD_ENABLE = yes
+COMBO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+
+LTO_ENABLE = yes
+
+SRC += features/achordion.c
