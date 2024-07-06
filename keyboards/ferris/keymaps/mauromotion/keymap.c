7416a320f5f9679e2382bce05fb8ed3820497c62
mauromotion <hello@mauromotion.com>
2024-07-06 19:03:21 +0100

    added LALT key to Adjust layer on Ferris Sweep

--- keyboards/ferris/keymaps/mauromotion/keymap.c
+++ keyboards/ferris/keymaps/mauromotion/keymap.c
@@ -125,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,---------------------------------------------.                   ,--------------------------------------------.
      XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F10,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-     XXXXXXX, KC_F4,   KC_F5,   KC_F6,   KC_F11,                       KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, KC_RCTL,
+     XXXXXXX, KC_F4,   KC_F5,   KC_F6,   KC_F11,                       KC_ACL0, KC_ACL1, KC_ACL2, KC_LALT, KC_RCTL,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
      XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F12,                       XXXXXXX, XXXXXXX, XXXXXXX, KC_CAPS, KC_NUM,
   //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
